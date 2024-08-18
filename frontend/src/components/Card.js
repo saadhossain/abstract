4c40a98aa17b4f2a42546e220911ebdfa1850b2c
saadhossain <mshsarkar@gmail.com>
2024-08-18 17:29:27 +0600

    Linked entire card to details page instead of title

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Card = ({ card }) => {
     return (
-        <div className='bg-[#F4F6F8] border border-[#CCD7E4] rounded-lg min-h-40'>
-            <h4 className='border-b border-[#CCD7E4] p-3 text-lg font-semibold'>
-                <Link to={`/cards/${card.title}`}>{card.title}</Link>
-                </h4>
+        <Link
+            to={`/cards/${card.title}`}
+            className='bg-[#F4F6F8] hover:bg-gray-200 border border-[#CCD7E4] rounded-lg min-h-40 duration-300 ease-in-out'>
+            <h4 className='border-b border-[#CCD7E4] p-3 text-lg font-semibold'>{card.title}</h4 >
             <p className='p-3 text-gray-600'>{card.description}</p>
-        </div>
+        </Link >
     );
 };
 
